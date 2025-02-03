ce5c22f115addfba6ff35830ebd82fd855b74853
Igor <iigore2061@gmail.com>
2025-02-03 16:27:34 +0200

    added notification model

--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import authenticate from "../middleware/authMiddleware";
+import authenticate from "../middleware/auth.middleware";
 import {
   sendNotification,
   getUserNotifications,
